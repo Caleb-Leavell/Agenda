97ca2e856f63e030bed642e9db519eec6e75a86c
Caleb <calebjleavell06@gmail.com>
2023-09-22 11:14:34 -0700

    Got the Scanner class working

--- agenda.java
+++ agenda.java
@@ -1,5 +1,13 @@
-public class Main {
+import java.util.Scanner;
+
+public class agenda {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    Scanner scnr = new Scanner(System.in);
+
+    System.out.print("Enter your message: ");
+    String input = scnr.nextLine();
+    System.out.println("");
+
+    System.out.println(input);
   }
 }
