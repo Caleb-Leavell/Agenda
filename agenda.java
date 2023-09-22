a52de5e556cf8f4206a3b7b044d891bc5d284b54
Caleb <calebjleavell06@gmail.com>
2023-09-22 11:29:23 -0700

    Added homescreen and ability to pick an option.

--- agenda.java
+++ agenda.java
@@ -4,10 +4,16 @@ public class agenda {
   public static void main(String[] args) {
     Scanner scnr = new Scanner(System.in);
 
-    System.out.print("Enter your message: ");
-    String input = scnr.nextLine();
+    int optionChosen;
+
+    //Display Home Screen
+    System.out.println("\nAGENDA\n\nInput Options:\n-View Current Tasks: 0\n-Add a new Task: 1\n");
+    
+    //Get Input
+    System.out.print("Choose Option: ");
+    optionChosen = scnr.nextInt();
     System.out.println("");
 
-    System.out.println(input);
+    System.out.println("You chose option " + optionChosen  + "!");
   }
 }
