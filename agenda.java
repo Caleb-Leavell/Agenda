b3be9a873389f46d2907d7466b87b11d1f772809
Caleb <calebjleavell06@gmail.com>
2023-09-25 09:29:03 -0700

    Attempt with OOP (didn't work)

--- agenda.java
+++ agenda.java
@@ -1,10 +1,29 @@
 import java.util.Scanner;
 
+class Task {
+    String name;
+    String description;
+    String deadlineDate;
+    String deadlineTime;
+
+    public Task(String name, String description, String deadlineDate, String deadlineTime) {
+      this.name = name;
+      this.description = description;
+      this.deadlineDate = deadlineDate;
+      this.deadlineTime = deadlineTime;
+    }
+
+    String returnName() {return name;}
+}
+
 public class agenda {
   public static void main(String[] args) {
     Scanner scnr = new Scanner(System.in);
 
     int optionChosen;
+    Task task1 = new Task("hw 1", "it's hw 1", "5/5/7", "5:00");
+
+    System.out.println(task1.returnName());
 
     //Display Home Screen
     System.out.println("\nAGENDA\n\nInput Options:\n-View Current Tasks: 0\n-Add a new Task: 1\n");
