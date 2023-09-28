b89cad5e9624eda7cfabb872c2dcb40f83bcaa75
Caleb <calebjleavell06@gmail.com>
2023-09-28 00:12:18 -0700

    Allow the user to add a new task name
    Attempt to display task names failed

--- agenda.java
+++ agenda.java
@@ -19,19 +19,59 @@ public class agenda {
   public static void main(String[] args) {
     Scanner scnr = new Scanner(System.in);
 
-    int optionChosen;
-    Task task1 = new Task("hw 1", "it's hw 1", "5/5/7", "5:00");
+    Task[] tasks = new Task[1];
 
-    System.out.println(task1.name);
-
-    //Display Home Screen
-    System.out.println("\nAGENDA\n\nInput Options:\n-View Current Tasks: 0\n-Add a new Task: 1\n");
+    home(scnr, tasks);
     
-    //Get Input
+  }
+
+  public static void displayTaskNames(Task[] tasks) {
+      if(tasks.length > 1) {
+        for(int i = 0; i < tasks.length - 1; i ++) {
+          System.out.println(tasks[i].name);
+        }
+      }
+      else {
+        System.out.println("No tasks to complete!");
+      }
+  }
+
+  public static void home(Scanner scnr, Task[] tasks) {
+    int optionChosen;
+
+    System.out.println("\nAGENDA\n\nInput Options:\n-View Current Tasks: 0\n-Add a new Task: 1\n");
+
     System.out.print("Choose Option: ");
     optionChosen = scnr.nextInt();
     System.out.println("");
 
-    System.out.println("You chose option " + optionChosen  + "!");
+    doAppNavigation(optionChosen, tasks, scnr);
+  }
+
+  public static void doAppNavigation(int optionChosen, Task[] tasks, Scanner scnr) {
+    if(optionChosen == 0) {
+      displayTaskNames(tasks);
+      home(scnr, tasks);
+    }
+
+    if(optionChosen == 1) {
+      tasks[tasks.length - 1] = new Task("", "", "", "");
+      System.out.print("Task Name: ");
+      tasks[tasks.length - 1].name = scnr.nextLine();
+      System.out.print("Task Description: ");
+      tasks[tasks.length - 1].description = scnr.nextLine();
+
+      Task[] tempList = new Task[tasks.length+1];
+      for(int i = 0; i < tasks.length; i ++) {
+        tempList[i] = tasks[i];
+      }
+      tasks = tempList;
+
+      home(scnr, tasks);
+    }
+
+    if(optionChosen == 4) {
+      return;
+    }
   }
 }
